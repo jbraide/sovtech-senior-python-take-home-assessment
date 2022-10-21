f4b219b448b0aad2e5fb60e406aed4107dbb20d1
Joseph Braide <braidej@gmail.com>
2022-10-21 11:57:55 +0100

    required dependencies and some config for graphql

--- swapi_graphql/swapi_graphql/settings.py
+++ swapi_graphql/swapi_graphql/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps
+    'api',
+
+    # third party packages
+    'graphene_django',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +129,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {
+    'SCHEMA': 'api.schema.schema',
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
